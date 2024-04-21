2399de76887601fe221ade4d46f59121381e0747
LisaDaVinchie <lisadavinchie@gmail.com>
2024-04-21 14:52:07 +0200

    Small correction on TestGetUserInput

--- app/src/test/java/mastermindgame_java/Settings/TestGetUserInput.java
+++ app/src/test/java/mastermindgame_java/Settings/TestGetUserInput.java
@@ -1,6 +1,5 @@
 package mastermindgame_java.Settings;
 
-import static mastermindgame_java.Settings.SetDifficulty.Level;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
